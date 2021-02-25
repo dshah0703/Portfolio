d8b37985df1e25468e30aed3497a299a04855c4c
Devanshi Shah <dshah163@my.centennialcollege.ca>
2021-02-25 13:56:41 +0530

    Buttons added
    description : added buttons of add edit and delete

--- server/routes/business.js
+++ server/routes/business.js
@@ -15,7 +15,7 @@ router.get('/',(req,res,next) => {
         else
         {
            // console.log(BusinessList);
-           res.render('business/list',{title: 'Business', BusinessList: businesslist})
+           res.render('business/list',{title: 'Business', BusinessList: businesslist});
         }
     });
 });
@@ -23,29 +23,101 @@ router.get('/',(req,res,next) => {
 module.exports = router;
 /* Get Route for Displaying the Add Page - Create Operation*/
 router.get('/add',(req,res,next) => {
-
+    res.render('business/add',{title: 'Add Business Contacts'});
+    
 });
 
 /* Post Route for Processing the Add Page -  Create Operation*/
 router.post('/add',(req,res,next) => {
 
+    let newBusiness = Business({
+        "name" : req.body.name,
+        "contact_number" :req.body.contact_number,
+        "email": req.body.email
+       
+    })
+    Business.create(newBusiness,(err, Business) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the business contact list
+            res.redirect('business/list');
+        }
+    })
 });
 
 
 /* Get Route for Displaying the Edit Page - Edit Operation*/
 router.get('/edit/id:',(req,res,next) => {
+    let id = req.params.id;
+
+    Business.findById(id,(err, business_contactToEdit) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('business/edit',{title:'Edit Business Contacts', business: businesscontactToEdit});
+        }
+
+    })
+
 
 });
 
 
 /* Post Route for Processing the Edit Page - Edit Operation*/
 router.post('/edit/id:',(req,res,next) => {
+    let updateBusiness = Business({
+        "id" : id,
+        "name" : req.body.name,
+        "contact_number" : req.body.contact_number,
+        "email" : req.body.email
+
+    });
+
+    Business.updateOne({_id: id},updateBusiness_contact,(err) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the business contact list again
+            res.redirect('/business-list');
+        }
+    });
+
 
 });
 
 
 /*Get method to perform Deletion - Delete Operation*/
 router.get('/delete/id:',(req,res,next) => {
+    let id = req.params.id;
+
+    Business.remove({_id:id},(err) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the business contact list
+            res.redirect('/business-list');
+
+        }
+    });
 
 });
+module.exports = router;
 
