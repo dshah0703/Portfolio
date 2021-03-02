1ea0db762e3c4de6a44c621f4e32fb47922e0948
Devanshi Shah <dshah163@my.centennialcollege.ca>
2021-03-03 01:40:11 +0530

    database connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/business_contact"
+    /*"URI": "mongodb://localhost/business_contact"*/
+    "URI": "mongodb+srv://devanshi:sAiX07vpW4Gtbjfw@cluster0.xoqqf.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
